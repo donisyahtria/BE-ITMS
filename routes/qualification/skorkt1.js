fe05704c6f49101ef6c68797ad5246c8fdb42f06
blue-sky <105834394+hrtnisri2016@users.noreply.github.com>
2024-05-28 14:57:24 +0700

    handle not found nippos di table skor

--- routes/qualification/skorkt1.js
+++ routes/qualification/skorkt1.js
@@ -12,12 +12,11 @@ router.post("/updateskor", async (req, res) => {
         const masukNilaiPerformance = await Promise.all(
             ambilNippos.map(async (filter) => {
                 const nilaiPerform = await prisma.skor_Performance.findFirst({
-                    where: {
-                        nippos: filter.nippos,
-                    }
+                    where: { nippos: filter.nippos }
                 });
-                const skor = nilaiPerform?.skor ?? 0;
-                const nilai = await prisma.talent_Qualification.updateMany({
+                if (!nilaiPerform) return null;
+                const skor = nilaiPerform.skor ?? 0;
+                return await prisma.talent_Qualification.updateMany({
                     where: {
                         AND: [
                             { nippos: filter.nippos },
@@ -26,23 +25,21 @@ router.post("/updateskor", async (req, res) => {
                     },
                     data: {
                         skor,
-                        berlaku_mulai: nilaiPerform?.Berlaku_Mulai,
-                        berlaku_hingga: nilaiPerform?.Berlaku_Hingga
+                        berlaku_mulai: nilaiPerform.Berlaku_Mulai,
+                        berlaku_hingga: nilaiPerform.Berlaku_Hingga
                     }
                 });
-                return nilai;
             })
         );
 
         const masukNilaiLA = await Promise.all(
             ambilNippos.map(async (filter) => {
                 const nilaiLA = await prisma.skor_LA.findFirst({
-                    where: {
-                        nippos: filter.nippos,
-                    }
+                    where: { nippos: filter.nippos }
                 });
-                const skor = nilaiLA?.skor ?? 0;
-                const nilai = await prisma.talent_Qualification.updateMany({
+                if (!nilaiLA) return null;
+                const skor = nilaiLA.skor ?? 0;
+                return await prisma.talent_Qualification.updateMany({
                     where: {
                         AND: [
                             { nippos: filter.nippos },
@@ -51,23 +48,21 @@ router.post("/updateskor", async (req, res) => {
                     },
                     data: {
                         skor,
-                        berlaku_mulai: nilaiLA?.Berlaku_Mulai,
-                        berlaku_hingga: nilaiLA?.Berlaku_Hingga
+                        berlaku_mulai: nilaiLA.Berlaku_Mulai,
+                        berlaku_hingga: nilaiLA.Berlaku_Hingga
                     }
                 });
-                return nilai;
             })
         );
 
         const masukNilaiakhlak = await Promise.all(
             ambilNippos.map(async (filter) => {
                 const nilaiakhlak = await prisma.skor_AKHLAK.findFirst({
-                    where: {
-                        nippos: filter.nippos,
-                    }
+                    where: { nippos: filter.nippos }
                 });
-                const skor = nilaiakhlak?.skor ?? 0;
-                const nilai = await prisma.talent_Qualification.updateMany({
+                if (!nilaiakhlak) return null;
+                const skor = nilaiakhlak.skor ?? 0;
+                return await prisma.talent_Qualification.updateMany({
                     where: {
                         AND: [
                             { nippos: filter.nippos },
@@ -76,31 +71,29 @@ router.post("/updateskor", async (req, res) => {
                     },
                     data: {
                         skor,
-                        berlaku_mulai: nilaiakhlak?.Berlaku_Mulai,
-                        berlaku_hingga: nilaiakhlak?.Berlaku_Hingga
+                        berlaku_mulai: nilaiakhlak.Berlaku_Mulai,
+                        berlaku_hingga: nilaiakhlak.Berlaku_Hingga
                     }
                 });
-                return nilai;
             })
         );
 
         const nilaibumn = await prisma.$queryRaw`
-             SELECT sb.nippos, sb.avg_skor
-             FROM "skor_BUMN" sb
-             RIGHT JOIN "Talent_Qualification" tq
-             on sb.nippos = tq.nippos
-             WHERE sb.nippos is not null
-             GROUP BY sb.nippos, sb.avg_skor`;
+            SELECT sb.nippos, sb.avg_skor
+            FROM "skor_BUMN" sb
+            RIGHT JOIN "Talent_Qualification" tq
+            ON sb.nippos = tq.nippos
+            WHERE sb.nippos IS NOT NULL
+            GROUP BY sb.nippos, sb.avg_skor`;
 
         const masukNilaiPotensi = await Promise.all(
             ambilNippos.map(async (filter) => {
                 const nilaipotensi = await prisma.skor_Potensi.findFirst({
-                    where: {
-                        nippos: filter.nippos,
-                    }
+                    where: { nippos: filter.nippos }
                 });
-                const skor = nilaipotensi?.skor ?? 0;
-                const nilai = await prisma.talent_Qualification.updateMany({
+                if (!nilaipotensi) return null;
+                const skor = nilaipotensi.skor ?? 0;
+                return await prisma.talent_Qualification.updateMany({
                     where: {
                         AND: [
                             { nippos: filter.nippos },
@@ -109,11 +102,10 @@ router.post("/updateskor", async (req, res) => {
                     },
                     data: {
                         skor,
-                        berlaku_mulai: nilaipotensi?.Berlaku_Mulai,
-                        berlaku_hingga: nilaipotensi?.Berlaku_Hingga
+                        berlaku_mulai: nilaipotensi.Berlaku_Mulai,
+                        berlaku_hingga: nilaipotensi.Berlaku_Hingga
                     }
                 });
-                return nilai;
             })
         );
 
@@ -137,19 +129,21 @@ router.post("/updateskor", async (req, res) => {
         );
 
         const nilaileadership = await prisma.$queryRaw`
-             SELECT sl.nippos, avg(sl.skor) as avg_skor
-             FROM "Skor_Leadership" sl
-             RIGHT JOIN "Talent_Qualification" tq
-             on sl.nippos = tq.nippos
-             GROUP BY sl.nippos`;
+            SELECT sl.nippos, AVG(sl.skor) AS avg_skor
+            FROM "Skor_Leadership" sl
+            RIGHT JOIN "Talent_Qualification" tq
+            ON sl.nippos = tq.nippos
+            GROUP BY sl.nippos`;
 
         const masukNilaileader = await Promise.all(
             nilaileadership.map(async (filter) => {
+                const nippos = filter.nippos;
+                if (!nippos) return null;
                 const skor = filter.avg_skor ?? 0;
                 return await prisma.talent_Qualification.updateMany({
                     where: {
                         AND: [
-                            { nippos: filter.nippos },
+                            { nippos },
                             { id_kriteria_penilaian: 2 }
                         ]
                     },
@@ -163,63 +157,63 @@ router.post("/updateskor", async (req, res) => {
         );
 
         const nilaitechnical = await prisma.$queryRaw`
-        WITH pre_talent_qualification AS (
-        SELECT 
-                nippos,
-                eventtalentid,
-                komite_unit,
-                '2024-04-30' AS CreatedAt
-        FROM "Talent_Profile" tp 
-        WHERE pakta_integritas=true
-                AND commitmenletter=true 
-),
-
-ranked_scores AS (
-        SELECT DISTINCT
-                tq.nippos,
-                tq.eventtalentid,
-                tq.komite_unit,
-                tq.createdat,
-                k.rumpun_jabatan,
-                komp.id_kompetensi_technical,
-                CASE 
+            WITH pre_talent_qualification AS (
+                SELECT 
+                    nippos,
+                    eventtalentid,
+                    komite_unit,
+                    '2024-04-30' AS CreatedAt
+                FROM "Talent_Profile" tp 
+                WHERE pakta_integritas=true
+                    AND commitmenletter=true 
+            ),
+            ranked_scores AS (
+                SELECT DISTINCT
+                    tq.nippos,
+                    tq.eventtalentid,
+                    tq.komite_unit,
+                    tq.createdat,
+                    k.rumpun_jabatan,
+                    komp.id_kompetensi_technical,
+                    CASE 
                         WHEN t.skor / komp."Advanced" > 1 THEN 1
                         ELSE t.skor / komp."Advanced"
-                END AS scaled_score,
-                t."Berlaku_Mulai",
-                t."Berlaku_Hingga",
-                ROW_NUMBER() OVER (
+                    END AS scaled_score,
+                    t."Berlaku_Mulai",
+                    t."Berlaku_Hingga",
+                    ROW_NUMBER() OVER (
                         PARTITION BY tq.nippos 
                         ORDER BY t."Berlaku_Mulai" DESC, 
-                                        (t.skor / komp."Advanced") DESC
-                ) AS rn
-        FROM pre_talent_qualification AS tq
-        LEFT JOIN "Karyawan" AS k
-                ON tq.nippos = k.nippos
-        LEFT JOIN "Kompetensi_Hardsoft_Rumpun" AS komp
-                ON komp.kode_rumpun_jabatan = k.rumpun_jabatan
-        LEFT JOIN "Skor_Technical" AS t
-                ON t.nippos = tq.nippos
-                AND t."id_Kompetensi" = komp."id_kompetensi_technical"
-        WHERE t.skor IS NOT NULL
-)
-
-SELECT 
-        nippos,
-        (AVG(scaled_score)*5) AS skor,
-        MAX("Berlaku_Mulai") AS berlaku_mulai,
-        MAX("Berlaku_Hingga") AS berlaku_hingga
-FROM ranked_scores
-WHERE rn <= 5
-GROUP BY nippos;`;
+                                 (t.skor / komp."Advanced") DESC
+                    ) AS rn
+                FROM pre_talent_qualification AS tq
+                LEFT JOIN "Karyawan" AS k
+                    ON tq.nippos = k.nippos
+                LEFT JOIN "Kompetensi_Hardsoft_Rumpun" AS komp
+                    ON komp.kode_rumpun_jabatan = k.rumpun_jabatan
+                LEFT JOIN "Skor_Technical" AS t
+                    ON t.nippos = tq.nippos
+                    AND t."id_Kompetensi" = komp."id_kompetensi_technical"
+                WHERE t.skor IS NOT NULL
+            )
+            SELECT 
+                nippos,
+                (AVG(scaled_score) * 5) AS skor,
+                MAX("Berlaku_Mulai") AS berlaku_mulai,
+                MAX("Berlaku_Hingga") AS berlaku_hingga
+            FROM ranked_scores
+            WHERE rn <= 5
+            GROUP BY nippos;`;
 
         const masukNilaitechnical = await Promise.all(
             nilaitechnical.map(async (filter) => {
+                const nippos = filter.nippos;
+                if (!nippos) return null;
                 const skor = filter.skor ?? 0;
                 return await prisma.talent_Qualification.updateMany({
                     where: {
                         AND: [
-                            { nippos: filter.nippos },
+                            { nippos },
                             { id_kriteria_penilaian: 3 }
                         ]
                     },
