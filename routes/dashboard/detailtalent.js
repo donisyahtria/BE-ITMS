3002ff0b8e9b71a9a92fbe3bc07de0f25f52ea87
blue-sky <105834394+hrtnisri2016@users.noreply.github.com>
2024-05-31 08:31:21 +0700

    add kategori matriks di detail dashboard

--- routes/dashboard/detailtalent.js
+++ routes/dashboard/detailtalent.js
@@ -6,30 +6,33 @@ const router = express.Router();
 router.get("/getdetailtalent", async (req, res) => {
   try {
     const getdetailtalent = await prisma.$queryRaw` 
-SELECT 
-    CAST(ROW_NUMBER() OVER () AS VARCHAR) AS id,
-    k.nama AS "nama", 
-    tp.nippos AS "nippos", 
-    CONCAT(rj.nama_jabatan, ' ', rb.nama_bagian) AS "posisi",
-    tp.leveljabatan AS "joblevel",
-    rrj.nama_rumpun_jabatan AS "jobfam",
-    rk.nama_kantor AS "nama_kantor",
-    et.nama_event AS "nama_event",
-    EXTRACT(YEAR FROM tp.dibuat_pada) AS "year"
-FROM 
-    talent_pool tp 
-LEFT JOIN 
-    "Karyawan" k ON tp.nippos = k.nippos 
-LEFT JOIN 
-    "Referensi_Rumpun_Jabatan" rrj ON tp.rumpunjabatan = rrj.kode_rumpun_jabatan 
-LEFT JOIN 
-    "Referensi_Kantor" rk ON k.kode_nopend = rk.nopend
-LEFT JOIN 
-    "Event_Talent" et ON tp.eventtalentid = et.id 
-LEFT JOIN 
-    "Referensi_Jabatan" rj ON k.kode_jabatan = rj.id
-LEFT JOIN 
-    "Referensi_Bagian" rb ON k.kode_bagian  = rb.id
+    SELECT 
+        CAST(ROW_NUMBER() OVER () AS VARCHAR) AS id,
+        k.nama AS "nama", 
+        tp.nippos AS "nippos", 
+        CONCAT(rj.nama_jabatan, ' ', rb.nama_bagian) AS "posisi",
+        tp.leveljabatan AS "joblevel",
+        rrj.nama_rumpun_jabatan AS "jobfam",
+        rk.nama_kantor AS "nama_kantor",
+        et.nama_event AS "nama_event",
+        EXTRACT(YEAR FROM tp.dibuat_pada) AS "year",
+        mk."Nama_Matriks_Kategori" as kategori_talent
+    FROM 
+        talent_pool tp 
+    LEFT JOIN 
+        "Karyawan" k ON tp.nippos = k.nippos 
+    LEFT JOIN 
+        "Referensi_Rumpun_Jabatan" rrj ON tp.rumpunjabatan = rrj.kode_rumpun_jabatan 
+    LEFT JOIN 
+        "Referensi_Kantor" rk ON k.kode_nopend = rk.nopend
+    LEFT JOIN 
+        "Event_Talent" et ON tp.eventtalentid = et.id 
+    LEFT JOIN 
+        "Referensi_Jabatan" rj ON k.kode_jabatan = rj.id
+    LEFT JOIN 
+        "Referensi_Bagian" rb ON k.kode_bagian  = rb.id
+    left join matriks_kategori mk  
+        on tp.id_matriks_kategori = mk."Id" 
     where et.evenstatus_id = 8;
  `;
 
