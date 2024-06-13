059c2b7689cd9164a4d766ebee28253af96330e4
arsyi <arsyilight@gmail.com>
2024-06-13 13:10:42 +0700

    update nki fix banget bray

--- routes/login/updateskornki.js
+++ routes/login/updateskornki.js
@@ -4,26 +4,64 @@ import express from "express";
 const router = express.Router();
 
 router.post("/updatenki", async (req, res) => {
-    try {
-        const nippos = req.body.nippos;
-        const nki_score = req.body.nki_score;
-        const year   = req.body.year;
+  try {
+    const { message } = req.body;
 
+    // Log the entire message array
+    console.log(message);
+    const array = message.message
+
+    // Create an array to hold the promises
+    const operations = array.map(async (item) => {
+      const { nippos, nki_score, year } = item;
+
+      // Find the record if it exists
+      const cariAda = await prisma.skor_Performance.findFirst({
+        where: {
+          nippos: nippos,
+          tahun: parseInt(year),
+        },
+        select: {
+          nippos: true,
+          skor: true,
+          tahun: true,
+        },
+      });
+
+      if (!cariAda) {
+        // Create a new record if it doesn't exist
+        const buatBaru = await prisma.skor_Performance.create({
+          data: {
+            nippos: nippos,
+            skor: parseFloat(nki_score),
+            tahun: parseInt(year),
+          },
+        });
+        return { type: "create", data: buatBaru };
+      } else {
+        // Update the existing record
         const updatePromises = await prisma.skor_Performance.updateMany({
-                where: {
-                    nippos: nippos
-                },
-                data: {
-                    skor: parseFloat(nki_score),
-                    tahun: parseInt(year)
-                }
-            });
+          where: {
+            nippos: nippos,
+            tahun: parseInt(year),
+          },
+          data: {
+            skor: parseFloat(nki_score),
+          },
+        });
+        return { type: "update", data: updatePromises };
+      }
+    });
 
-        res.status(200).json({ message: "Success", updatePromises });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Internal server error" });
-    }
+    // Wait for all operations to complete
+    const results = await Promise.all(operations);
+
+    // Respond with the results
+    res.status(200).json({ message: "Success", results });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 export default router;
